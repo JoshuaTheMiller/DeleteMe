d0a5cf910e7bc44027ce08538e913ab5e6661ca2
Joshua Miller <joshua.d.miller@optum.com>
2024-04-08 13:09:02 -0500

    Revert all commits back to 0451e4a

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,48 +8,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-    private final HttpClient client = HttpClient.newHttpClient();
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
     @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) throws Exception {
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) throws MalformedURLException {
 
         String someConfigurationValue = System.getenv("NA");
 
-        String validatedUri = ValidateUri(name);
-
-        URI uri = new URI(validatedUri);
-
-        HttpRequest r = HttpRequest.newBuilder(uri).build();
+        // Trying to force a specific CodeQL warning to demonstrate an issue
+        URL what = new URL(someConfigurationValue + name);
 
         try{
-            client.send(r, null);
+            what.openConnection();
         }
         catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
 
         return String.format("Hello %s!", name);
-    }
-
-    private String ValidateUri(String name) throws Exception {
-        // Doesn't fix the problem
-        if(name.contains("/")) {
-            throw new Exception("Bad");
-        }
-
-        return "https://example.com/" + name;
     }
 }
