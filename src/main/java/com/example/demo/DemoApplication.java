b98ad9ac6b6b5648525b1a8e8847c19d3c6cb7ad
Joshua Miller <joshua.d.miller@optum.com>
2024-04-08 13:28:54 -0500

    Add simple validation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -23,6 +23,10 @@ public class DemoApplication {
 
         String someConfigurationValue = System.getenv("NA").isEmpty() ? "example.com" : System.getenv("NA");
 
+        if(name.contains("/")) {
+            return "bad";
+        }
+
         // Trying to force a specific CodeQL warning to demonstrate an issue
         URL what = new URL("https://", someConfigurationValue, 443, name);
 
