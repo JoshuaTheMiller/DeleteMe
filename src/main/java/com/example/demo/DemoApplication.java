40ff82882b900994f6b6332f2afb136bce211fe5
Joshua Miller <joshua.d.miller@optum.com>
2024-04-08 11:38:50 -0500

    Use totally different classes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,18 +8,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+    private final HttpClient client = HttpClient.newHttpClient();
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
     @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) throws MalformedURLException {
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) throws IOException, URISyntaxException, InterruptedException {
 
         String someConfigurationValue = System.getenv("NA");
 
@@ -28,20 +33,19 @@ public class DemoApplication {
             return "Bad";
         }
 
-
-        // Trying to force a specific CodeQL warning to demonstrate an issue
-        URL what = new URL(someConfigurationValue + name);
+        URI uri = new URI(someConfigurationValue + name);
+        HttpRequest r = HttpRequest.newBuilder().build();
 
         // Seems pretty... silly that we would validate the URL starting
         // with something when we can clearly see how it is set.
         // Perhaps using the URL class is simply a bad choice (the constructor
         // has been deprecated, after all.
-        if(!what.getHost().startsWith("https://example.com")){
+        if(!uri.getHost().startsWith("https://example.com")){
             return "Bad";
         }
 
         try{
-            what.openConnection();
+            client.send(r, null);
         }
         catch (IOException ex) {
             System.out.println(ex.getMessage());
